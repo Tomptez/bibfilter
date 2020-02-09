ee9d2ad47c23526d5836707db54a6c992898eff9
Tom <t-knuf@hotmail.de>
2020-02-09 23:49:13 +0100

    added python-dotenv

--- bibfilter/__init__.py
+++ bibfilter/__init__.py
@@ -3,7 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 from flask_basicauth import BasicAuth
+from dotenv import load_dotenv
 import os
+load_dotenv()
 
 def get_env_variable(name):
     try:
