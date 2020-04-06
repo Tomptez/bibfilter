bcf94d69ae80dc43ca5fbfdfce6d1f46f4b136c0
Tom Knuf <t-knuf@hotmail.de>
2020-04-06 14:58:45 +0200

    Improve DOI_lookup code

--- bibfilter/DOI_lookup.py
+++ bibfilter/DOI_lookup.py
@@ -8,6 +8,20 @@ from sqlalchemy.orm import sessionmaker
 from bibfilter import db
 from bibfilter.models import Article
 
+def get_json_value(the_json, key_list, default_value):
+    # get_json_value(result, ["title",0], "")
+    # returns result["title"][0] if existing, otherwise uses default value ""
+
+    current = the_json # start from the base json
+    for key in key_list:
+        if isinstance(current, dict) and key in current:
+            current = current[key] # move to dictionary value
+        elif isinstance(current, list) and key < len(current):
+            current = current[key] # move to item in a list
+        else:
+            return default_value
+    return current # return whatever value we reached
+
 def add_item(doi):
     r = requests.get(f"https://api.crossref.org/works/{doi}")
 
@@ -21,53 +35,37 @@ def add_item(doi):
         #fields = ["title", "URL", "publisher", "type", "author", "published-print", "container-title", "volume", "issue"]
 
         try:
-            if result.get("title") != None and len(result.get("title")) > 0:
-                title = result["title"][0]
+            title = get_json_value(result, ["title",0],"")
+
+            if result.get("author") != None and len(result.get("author")) > 0:
+                author = f'{get_json_value(result, ["author", 0, "family"], "")}, {get_json_value(result, ["author", 0, "given"], "")}'
             else:
-                title = ""
+                author = f'{get_json_value(result, ["editor", 0, "family"], "")}, {get_json_value(result, ["editor", 0, "given"], "")}'
+            
+            year = get_json_value(result, ["published-print", "date-parts", 0, 0], "")
+            journal = get_json_value(result, ["container-title", 0], "")
 
             url = result.get("URL")
             publisher = result.get("publisher")
             ENTRYTYPE = result.get("type")
-
-            if result.get("author") != None and len(result.get("author")) > 0:
-                author = f'{result["author"][0].get("family","")}, {result["author"][0].get("given","")}'
-            else:
-                if result.get("editor") != None and len(result.get("editor")) > 0:
-                    author = f'{result["editor"][0].get("family","")}, {result["editor"][0].get("given","")}'
-                else:
-                    author = ""
-
-            if result.get("published-print") != None and len(result.get("published-print")) > 0:
-                year = str(result["published-print"]["date-parts"][0][0])
-            else:
-                year = ""
-
-            if result.get("container-title") != None and len(result.get("container-title")) > 0:
-                journal = result["container-title"][0]
-            else:
-                journal = ""
-
-            # pages = db.Column(db.String)
             volume = result.get("volume")
             number = result.get("issue")
+            # pages = db.Column(db.String)
 
-            if result.get("author") != None and len(result.get("author")) > 0:
-                ID = title.split()[1]+result["author"][0]["family"]+year
-            else:
-                ID = title.split()[1]+doi.replace("/","").replace(".","")
+            ID = title.split()[1]+doi.replace("/","").replace(".","")
+
         except Exception as e:
             print("ERROR", e)
 
+        # Create the session
+        session = db.session()
         if title == "" or author == "":
             return f"Couldn't receive required metadata of {doi}"
         elif len(list(session.query(Article).filter(Article.ID == ID))) > 0:
             return f"{title} already exists in the database."
 
         new_art = Article(title=title, url=url, publisher=publisher, ID=ID, ENTRYTYPE=ENTRYTYPE, author=author, year=year, doi=doi, journal=journal, volume=volume, number=number, _date_created_int = time.time(), _date_created_str = str(datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S")))
-
-        #Create the session
-        session = db.session()
+        # print(title, author, year, journal, volume, url, publisher, ID, ENTRYTYPE)
         session.add(new_art)
         session.commit()
 
@@ -75,4 +73,4 @@ def add_item(doi):
 
 if __name__ == "__main__":
     doi = "10.1007/978-3-319-69626-3"
-    add_item(doi)
+    print(add_item(doi))
