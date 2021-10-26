54e8cde1e9ac2b299a151db9c3457e3e6b3c8ac3
Tom Knuf <t-knuf@hotmail.de>
2021-10-26 16:50:34 +0200

    Fixed error due to comit mistake

--- Analyze_content_for_search.py
+++ Analyze_content_for_search.py
@@ -33,55 +33,6 @@ while not connected:
         print("Elasticsearch not available yet, trying again in 2s...")
         time.sleep(2)
 
-    
-elasticMapping = {
-    "mappings": {
-        "properties": {
-            "dbid": {"type": "text"},
-            "ID": {"type": "text"},
-            "ENTRYTYPE": {"type": "text"},
-            "title": {"type": "text", "term_vector": "with_positions_offsets"},
-            "author": {"type": "text", "term_vector": "with_positions_offsets"},
-            "authorlast": {"type": "text"},
-            "year": {"type": "text"},
-            "journal": {"type": "text"},
-            "publication": {"type": "text"},
-            "booktitle": {"type": "text"},
-            "isbn": {"type": "text"},
-            "issn": {"type": "text"},
-            "doi": {"type": "text"},
-            "pages": {"type": "text"},
-            "volume": {"type": "text"},
-            "number": {"type": "text"},
-            "tags": {"type": "text"},
-            "icon": {"type": "text"},
-            "notes": {"type": "text"},
-            "abstract": {"type": "text", "term_vector": "with_positions_offsets"},
-            "editor": {"type": "text"},
-            "tags_man": {"type": "text"},
-            "tags_auto": {"type": "text"},
-            "extra": {"type": "text"},
-            "journal_abbrev": {"type": "text"},
-            "address": {"type": "text"},
-            "institution": {"type": "text"},
-            "publisher": {"type": "text"},
-            "language": {"type": "text"},
-            "url": {"type": "text"},
-            "articleFullText": {"type": "text", "term_vector": "with_positions_offsets"},
-            "importantWords": {"type": "text"},
-            "contentChecked": {"type": "text"},
-            "elasticIndexed": {"type": "text"},
-            "references": {"type": "text"},
-            "searchIndex": {"type": "text"},
-            "date_added": {"type": "text"},
-            "date_modified": {"type": "text"},
-            "date_last_zotero_sync": {"type": "text"},
-            "date_modified_pretty": {"type": "text"},
-            "date_added_pretty": {"type": "text"},
-        }
-    }
-}
-
 # Converts SQL article to dict to insert in elasticsearch
 def row2dict(row):
         d = {}
---
+++ bibfilter/functions.py
@@ -0,0 +1,64 @@
+import os
+from elasticsearch import Elasticsearch
+
+elasticMapping = {
+    "mappings": {
+        "properties": {
+            "dbid": {"type": "text"},
+            "ID": {"type": "text"},
+            "ENTRYTYPE": {"type": "text"},
+            "title": {"type": "text", "term_vector": "with_positions_offsets"},
+            "author": {"type": "text", "term_vector": "with_positions_offsets"},
+            "authorlast": {"type": "text"},
+            "year": {"type": "text"},
+            "journal": {"type": "text"},
+            "publication": {"type": "text"},
+            "booktitle": {"type": "text"},
+            "isbn": {"type": "text"},
+            "issn": {"type": "text"},
+            "doi": {"type": "text"},
+            "pages": {"type": "text"},
+            "volume": {"type": "text"},
+            "number": {"type": "text"},
+            "tags": {"type": "text"},
+            "icon": {"type": "text"},
+            "notes": {"type": "text"},
+            "abstract": {"type": "text", "term_vector": "with_positions_offsets"},
+            "editor": {"type": "text"},
+            "tags_man": {"type": "text"},
+            "tags_auto": {"type": "text"},
+            "extra": {"type": "text"},
+            "journal_abbrev": {"type": "text"},
+            "address": {"type": "text"},
+            "institution": {"type": "text"},
+            "publisher": {"type": "text"},
+            "language": {"type": "text"},
+            "url": {"type": "text"},
+            "articleFullText": {"type": "text", "term_vector": "with_positions_offsets"},
+            "importantWords": {"type": "text"},
+            "contentChecked": {"type": "text"},
+            "elasticIndexed": {"type": "text"},
+            "references": {"type": "text"},
+            "searchIndex": {"type": "text"},
+            "date_added": {"type": "text"},
+            "date_modified": {"type": "text"},
+            "date_last_zotero_sync": {"type": "text"},
+            "date_modified_pretty": {"type": "text"},
+            "date_added_pretty": {"type": "text"},
+        }
+    }
+}
+
+# Check if the elasticsearch environment variable is set, if a connection is possible and bibfilter-index exists
+def elasticsearchCheck():    
+    if os.environ.get("USE_ELASTICSEARCH").upper() == "TRUE":
+        try:
+            es = Elasticsearch(host="localhost", port=9200)
+            if not es.indices.exists(index="bibfilter-index"):
+                es.indices.create(index="bibfilter-index", body=elasticMapping)
+            useElasticSearch = True
+            es.close()
+        except Exception as e:
+            print(e)
+            useElasticSearch = False
+    return useElasticSearch
